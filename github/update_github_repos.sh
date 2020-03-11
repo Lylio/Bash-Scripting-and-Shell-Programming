db6eaf81fd40d09d3b41130bb022291f3b43dff2
Lyle Christine <cxz@gmx.com>
2020-03-11 22:31:05 +0000

    added more enviroment variabe questions

--- github/update_github_repos.sh
+++ github/update_github_repos.sh
@@ -3,13 +3,11 @@
 repo_name=$1
 branch_name=$2
 
+read -p "Please enter username: " username
+read -s -p "Please enter password: " password
+read -p "Please enter repo name: " repo_name
+read -p "Please enter ticket/branch name: " branch_name
 rm -rf ~/scratch
-
-#Test if environment variables have bee passed
-if [[ -z "$1" ||  -z "$2" ]]; then
-    echo Error: missing parameters. Please include repository and branch environment variables.
-    exit 1
-fi
 
 #Clone repo
 git clone git@github.com:Lylio/${repo_name}.git ~/scratch/${repo_name}
@@ -19,9 +17,9 @@ message=$(tail -n +7 test_successful.txt)
 cd ~/scratch/$repo_name
 echo ${message} >> README.md
 
-git checkout, commit, push
+#git checkout, commit, push
 git checkout -b $branch_name
-git commit -a -m "$branch_name : MINOR : added test file"
+git commit -a -m "$branch_name : MINOR : upated README.md"
 git push --set-upstream origin $branch_name
 
 #Last words
