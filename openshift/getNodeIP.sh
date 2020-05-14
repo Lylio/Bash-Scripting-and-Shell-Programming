170162abf50a9557f91ee438ab8d65b34294cc2e
Lylio <cxz@gmx.com>
2020-05-14 08:50:20 +0100

    update

--- openshift/getNodeIP.sh
+++ openshift/getNodeIP.sh
@@ -2,21 +2,21 @@
 # A script to export the IP addresses of all running containers
 # WORK IN PROGRESS
 
-OPENSHIFTS="$1"
+OPENSHIFTS="$*"
 
 read -p "OpenShift user: " USER
 read -s -p "OpenShift pwd: " PWD
 echo
-echo "your details are $USER and $PWD"
 
 if [ -z "$OPENSHIFTS" ]; then
-    OPENSHIFTS="master.openshift.local:8443"
+    OPENSHIFTS="https://master.openshift.local:8443"
 fi
-
-echo "OPENSHIFTS = $OPENSHIFTS"
 
 rm -rf /tmp/ip_list.txt
 
+oc project kiosk-concession
 for openshift in $OPENSHIFTS; do
-echo xxxx USING XXXXX ${openshift}
+    echo XXX USING ${openshift} XXXX
+    oc login openshift --username=$USER --password=$PWD
+    oc describe pod | grep -w Name | awk '{ print $2 }' && oc describe pod | grep IP | awk '{ print $2 }'
 done
